591f79ecd4fbdc206c6b9ebce187f1f52637c228
kurs <kurs@DESKTOP-V3HDKPQ>
2023-05-27 15:42:11 +0200

    update

--- ReflectionExample/Program.cs
+++ ReflectionExample/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +10,7 @@ namespace ReflectionExample
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			ReflectionTest.fieldA = 1;
 			ReflectionTest.fieldB = 1.23;
@@ -16,8 +18,54 @@ namespace ReflectionExample
 			ReflectionTest.fieldD = true;
 			//ReflectionTest.Lookup();
 
-			CreateObject();
+			//CreateObject();
 
+			// ewaluacja kodu c# w czasie rzeczywistym
+			string code = "var a=9; if (a>=9) { return 1;} else { return -10; } ";
+			int result = await CSharpScript.EvaluateAsync<int>(code);
+            Console.WriteLine(result);
+
+            var assembly = Assembly.LoadFrom("ExternalLibrary.dll");
+			foreach (var type in assembly.ExportedTypes)
+			{
+                Console.WriteLine("===================");
+                Console.WriteLine(type.FullName);
+
+				foreach (var ctr in type.GetConstructors())
+				{
+                    Console.WriteLine($"Konstruktor: {ctr.Name}");
+					foreach (var ctrParam in ctr.GetParameters())
+					{
+                        Console.WriteLine($"{ctrParam.Name} : {ctrParam.ParameterType}");
+                    }
+                }
+
+				Console.WriteLine("==================");
+				Console.WriteLine("Properties");
+				foreach (var prop in type.GetProperties())
+				{
+					Console.WriteLine($"{prop.Name} - {prop.PropertyType}");
+				}
+
+				Console.WriteLine("==================");
+				Console.WriteLine("Methods");
+				foreach (var method in type.GetMethods())
+				{
+					Console.WriteLine($"{method.Name} - {method.ReturnType}");
+				}
+			}
+
+			Console.ReadKey();
+
+		}
+
+		public class DemoClass
+		{
+			private readonly double constValue = 1.23456;
+			public double Show()
+			{
+				return constValue;
+			}
 		}
 
 		static void CreateObject()
@@ -27,7 +75,47 @@ namespace ReflectionExample
 			ReflectionClass castedClass = obj as ReflectionClass;
 			if (castedClass != null)
 			{
-                Console.WriteLine($"{castedClass.fieldA}, {castedClass.fieldB}");
+				Console.WriteLine($"{castedClass.fieldA}, {castedClass.fieldB}");
+			}
+
+			object[] arguments = new object[] { "Hello world!" };
+			obj = Activator.CreateInstance(type, arguments);
+			castedClass = obj as ReflectionClass;
+			if (castedClass != null)
+			{
+				Console.WriteLine($"{castedClass.fieldA}, {castedClass.fieldB}");
+			}
+
+			// zmiana wartości dla pola
+			PropertyInfo property = type.GetProperty("fieldB");
+			if (property != null)
+			{
+				property.SetValue(castedClass, "ABCDEFGHIJKL");
+				string newValue = property.GetValue(castedClass) as string;
+				Console.WriteLine(newValue);
+			}
+
+			//wywołanie metody przy pomocy reflekacji
+			MethodInfo method = type.GetMethod("MethodStr");
+			if (method != null)
+			{
+				string result = (string)method.Invoke(castedClass, new object[0]);
+				Console.WriteLine(result);
+			}
+
+			method = type.GetMethod("MethodInt");
+			if (method != null)
+			{
+				int x = (int)method.Invoke(castedClass, new object[] { 10 });
+                Console.WriteLine(x);
+            }
+
+			DemoClass dc = new DemoClass();
+			FieldInfo field = typeof(DemoClass).GetField("constValue", BindingFlags.NonPublic | BindingFlags.Instance);
+			if (field!=null)
+			{
+				field.SetValue(dc, -9.8765);
+                Console.WriteLine(dc.Show());
             }
 		}
 	}
