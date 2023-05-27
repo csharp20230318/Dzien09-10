692831a7b6996bac069b60e828d7e5dc871641b8
kurs <kurs@DESKTOP-V3HDKPQ>
2023-05-27 12:05:41 +0200

    Add project files.

---
+++ NLogTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogTest
+{
+	internal class Program
+	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+
+		static void Main(string[] args)
+		{
+			logger.Trace("Trace");
+			logger.Debug("Debug");
+			logger.Info("Info");
+			logger.Warn("Warn");
+			logger.Error("Error");
+			logger.Fatal("Fatal");
+			Console.ReadKey();
+		}
+	}
+}
