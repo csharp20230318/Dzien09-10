e6bf0107aa9db20e4992bc27e1458af73c9ae1ed
kurs <kurs@DESKTOP-V3HDKPQ>
2023-05-27 13:04:13 +0200

    update

---
+++ ReflectionExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExample
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ReflectionTest.fieldA = 1;
+			ReflectionTest.fieldB = 1.23;
+			ReflectionTest.fieldC = "AAAA";
+			ReflectionTest.fieldD = true;
+			//ReflectionTest.Lookup();
+
+			CreateObject();
+
+		}
+
+		static void CreateObject()
+		{
+			Type type = typeof(ReflectionClass);
+			object obj = Activator.CreateInstance(type);
+			ReflectionClass castedClass = obj as ReflectionClass;
+			if (castedClass != null)
+			{
+                Console.WriteLine($"{castedClass.fieldA}, {castedClass.fieldB}");
+            }
+		}
+	}
+}
---
+++ ReflectionExample/ReflectionTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExample
+{
+	public static class ReflectionTest
+	{
+		public static int fieldA;
+		public static double fieldB;
+		public static string fieldC;
+		public static bool fieldD;
+		private static DateTime fieldE = DateTime.UtcNow;
+
+
+		public static int Test1() { return 1; }
+		private static double Test2() { return 1.23; }
+		public static bool Test3() { return false; }
+
+		public static void Lookup()
+		{
+			Type type = typeof(ReflectionTest);
+			FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+			foreach (var field in fields)
+			{
+				string name = field.Name;
+				object obj = field.GetValue(null);
+
+                Console.WriteLine($"[{name}] - {obj}");
+            }
+
+			MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+			foreach (var method in methods)
+			{
+                Console.WriteLine($"[{method.Name}] - {method.ReturnType}");
+            }
+		}
+	}
+}
