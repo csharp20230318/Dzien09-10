4724d57e8d498ad1766b06aa087c9cb376a82d93
kurs <kurs@DESKTOP-V3HDKPQ>
2023-05-28 10:31:12 +0200

    zeromq

--- PubSub/Program.cs
+++ PubSub/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,24 @@ namespace PubSub
 	{
 		static void Main(string[] args)
 		{
+			Publisher2 pub2 = new Publisher2();
+			pub2.OnChange += Pub2_OnChange;
+			pub2.OnChange += (sender, e) => Console.WriteLine($"a tu odebrano: {e.Value}");
+			pub2.Raise(123);
+			pub2.Raise(456);
+			Console.ReadKey();
+
 			Publisher1 pub1 = new Publisher1();
 			pub1.OnChange += Pub1_OnChange;
 			pub1.OnChange += () => Console.WriteLine("Kolejne wywołanie z Publisher 1");
 			pub1.Raise();
 			Console.ReadKey();
 		}
+
+		private static void Pub2_OnChange(object sender, MyEventArgs e)
+		{
+            Console.WriteLine($"Odebrana wartość: {e.Value}");
+        }
 
 		private static void Pub1_OnChange()
 		{
@@ -40,7 +53,12 @@ namespace PubSub
 
 	class Publisher2
 	{
+		public event EventHandler<MyEventArgs> OnChange = delegate { };
 
+		public void Raise(int val)
+		{
+			OnChange(this, new MyEventArgs() { Value = val });
+		}
 	}
 
 
---
+++ ZeroMQ/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace ZeroMQ
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			new Thread(
+				() =>{
+					// implementacja
+					Console.WriteLine("Odpalam publishera");
+					Random rnd = new Random();
+
+					using (PublisherSocket pubSocket = new PublisherSocket())
+					{
+						// przypięcie do portu TCP
+						pubSocket.Bind("tcp://*:12345");
+						for (int i = 0; i < 100; i++)
+						{
+							string topic = rnd.NextDouble() > 0.5 ? "topicA" : "topicB";
+							string msg = $"{topic} - {i}";
+							pubSocket.SendMoreFrame(topic).SendFrame(msg);
+                            Console.WriteLine($"PUBLISHER: {msg}");
+							Thread.Sleep(1000);
+                        }						
+					}
+				}
+			).Start();
+
+			// implementacja subskrybenta
+			using (var subSocket = new SubscriberSocket())
+			{
+				subSocket.Connect("tcp://localhost:12345");
+				//subSocket.SubscribeToAnyTopic();
+				subSocket.Subscribe("topicA");
+                Console.WriteLine("odbieram komunikaty");
+				while (true)
+				{
+					string topic = subSocket.ReceiveFrameString();
+					string msg = subSocket.ReceiveFrameString();
+                    Console.WriteLine($"odebrano : {topic} | {msg}");
+                }
+            }
+
+			Console.ReadKey();
+
+		}
+	}
+}
